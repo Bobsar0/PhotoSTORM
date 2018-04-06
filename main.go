1b9e8c1e0d8fabdb2eb8940136f0640bba0fcd0b
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-04-06 21:42:17 +0100

    Adding gorilla/mux router & breaking up handleFunc into customized functions

--- main.go
+++ main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"fmt"      //package used to format strings and output them to different places.
 	"net/http" //package used to both create an app capable of responding to web requests and making web requests to other servers.
+
+	"github.com/gorilla/mux" //The gorrila/mux router
 )
 
-//handler function processes incoming web requests and determines what to return to the client
-func handlerFunc(w http.ResponseWriter, r *http.Request) { // 'w' allows us to modify response to client. 'r' is used to access data from the web request
+//'home' handler function processes incoming web requests to access home page
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" { // if home page
-		fmt.Fprint(w, "<h1>Welcome to PhotoSTORM.com!</h1>")
-	} else if r.URL.Path == "/contact" { //if contact page
-		fmt.Fprint(w, "To get in touch, please send an email "+
-			"to <a href=\"mailto:support@PhotoSTORM.com\">"+
-			"support@PhotoSTORM.com</a>.")
-	} else { 
-		w.WriteHeader(http.StatusNotFound) //404
-		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1>"+
-			"<p>Please email us if you keep being sent to an "+
-			"invalid page.</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to PhotoSTORM.com!</h1>")
+}
+
+//'contact' handler function processes incoming web requests to access contact page
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@PhotoSTORM.com\">"+
+		"support@PhotoSTORM.com</a>.")
+}
+
+//'faq' handler function processes incoming web requests to access faq page
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Check back for <b>Frequently Asked Questions.</b>")
 }
 func main() {
-	http.HandleFunc("/", handlerFunc) // handlerFunc is set as the function to handle web requests going to our server with the path '/'.
-	http.ListenAndServe(":8080", nil) // starts up a web server listening on port 8080 using the default http handlers identified by 'nil'.
+	r := mux.NewRouter()              //New gorilla/mux router
+	r.HandleFunc("/", home)           // tells the router to call the home function when the user wants to visit home page - indicated by path '/'
+	r.HandleFunc("/contact", contact) // tells the router to call the home function when the user wants to visit contact page - indicated by path '/contact'
+	r.HandleFunc("/faq", faq)         // tells the router to call the home function when the user wants to visit faq page - indicated by path '/faq'
+
+	http.ListenAndServe(":8080", r) // starts up a web server listening on port 8080 using our gorilla/mux router as the default handler for web requests.
+
 }
