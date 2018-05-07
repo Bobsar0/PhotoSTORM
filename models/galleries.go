0fe8243878d0c6e7cf6a2dfd6932cd0ec5e697d5
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-05-07 02:37:14 +0100

    Validating Galleries

--- models/galleries.go
+++ models/galleries.go
@@ -24,8 +24,14 @@ type galleryService struct {
 type galleryValidator struct {
 	GalleryDB
 }
+type galleryValFn func(*Gallery) error //gallery validation functions
 
 var _ GalleryDB = &galleryGorm{} // ensure that our galleryGorm always implements our GalleryDB interface.
+
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+	ErrTitleRequired  modelError = "models: title is required"
+)
 
 func NewGalleryService(db *gorm.DB) GalleryService {
 	return &galleryService{
@@ -35,6 +41,41 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 			},
 		},
 	}
+}
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//Ensures a userID is set automatically
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+//Ensures user inputs title
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
 }
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
